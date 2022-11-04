f5f53f61e3020b4c308fc73b0214d06c2f0f806e
sial <rimsila.itc@gmail.com>
2022-11-04 09:54:58 +0700

    fix: icon eslint

--- src/types/icons.d.ts
+++ src/types/icons.d.ts
@@ -1,4 +1,9 @@
-// Manual export global type
+//@ts-nocheck
+
+/**
+ * auto import will generate type only when we save so when we run dev it will remove those type.
+ * to avoid eslint mark it error we need to Manual icon type here
+ */
 
 export {}
 
