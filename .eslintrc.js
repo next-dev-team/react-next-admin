8c09b8347f5cfd50be5a21bef36a0550ca620948
sial <rimsila.itc@gmail.com>
2022-11-06 00:35:25 +0700

    feat: graph ql

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: [require.resolve('umi/eslint')],
+  extends: [require.resolve('umi/eslint'), 'plugin:valtio/recommended'],
   rules: {
     // '@typescript-eslint/no-unused-vars': 0,
     'react/jsx-no-undef': [0, { allowGlobals: true }],
---
+++ src/graphql/operations.tsx
@@ -0,0 +1,15 @@
+import * as Types from './schemas'
+
+export type PostQueryVariables = Types.Exact<{
+  id: Types.Scalars['ID']
+}>
+
+export type PostQuery = {
+  __typename?: 'Query'
+  post?: {
+    __typename?: 'Post'
+    id?: string | null
+    title?: string | null
+    body?: string | null
+  } | null
+}
